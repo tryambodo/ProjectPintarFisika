4196bf808d8e22facb0d0917443337253e9ae218
Bagas Try Ambodo <bagasambodo@gmail.com>
2017-03-22 13:08:20 +0700

    Commit (splash screen)

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl305091429/pintarfisikasmasmk/SplashScreen.java
@@ -0,0 +1,34 @@
+package id.sch.smktelkom_mlg.project2.xirpl305091429.pintarfisikasmasmk;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Thread timerThread = new Thread() {
+            public void run() {
+                try {
+                    sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+}
